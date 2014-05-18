85600b4cb25f5325fc65758316834ea2b8ecfdf6
AmanoShota <amanoshota15@gmail.com>
2014-05-19 07:23:40 +0900

    Initial Commit

---
+++ shageki/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shageki
+//
+//  Created by AmanoShota on 2014/05/19.
+//  Copyright (c) 2014年 ShotaAmano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shageki/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shageki
+//
+//  Created by AmanoShota on 2014/05/19.
+//  Copyright (c) 2014年 ShotaAmano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
